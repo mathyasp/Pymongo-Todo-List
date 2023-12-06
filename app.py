a74142a71f23e159bee11380004f621e7b064546
m p <mtpap@MacBook-Pro.local>
2023-12-05 21:07:53 -0500

    completed stretch challenges

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -9,10 +10,9 @@ client = MongoClient('localhost', 27017)
 db = client.flask_db
 todos = db.todos
 
-
 @app.route('/', methods=('GET', 'POST'))
 def index():
-    if request.method=='POST':
+    if request.method == 'POST':
         content = request.form['content']
         degree = request.form['priority']
         order = 0
@@ -22,10 +22,11 @@ def index():
             order = 2
         else:
             order = 3
-        todos.insert_one({'content': content, 'priority': degree, 'order': order})
+        complete = False 
+        current_date = datetime.now(timezone.utc)
+        todos.insert_one({'content': content, 'priority': degree, 'order': order, 'complete': complete, 'date': current_date}) 
         return redirect(url_for('index'))
 
-    all_todos = todos.find()
     all_todos = todos.find().sort('order')
     return render_template('index.html', todos=all_todos) 
 
@@ -33,4 +34,12 @@ def index():
 @app.post('/<id>/delete/')
 def delete(id):
     todos.delete_one({"_id": ObjectId(id)})
+    return redirect(url_for('index'))
+
+
+@app.post('/<id>/complete/')
+def complete(id):
+    filter = {"_id": ObjectId(id)}
+    new_values = {"$set": {"complete": True}}
+    todos.update_one(filter, new_values)
     return redirect(url_for('index'))
